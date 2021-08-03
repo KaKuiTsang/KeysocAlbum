d5e15ab3f585168d827f2a9dac246becebade86c
Tsang Ka Kui <kakuitsang2010@gmail.com>
2021-08-03 19:13:20 +0800

    init project

---
+++ KeysocAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KeysocAlbum
+//
+//  Created by Tsang Ka Kui on 3/8/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
